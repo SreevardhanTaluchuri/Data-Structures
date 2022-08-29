e9953a59ab6c422bdc89d84158e105c809ebadf4
Sreevardhan Taluchuri <sreevardhan.t@virinnovations.in>
2022-08-29 22:14:53 +0530

    Data Structures Init

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,11 @@
+public class LinearSearch {
+  public static void main(String[] args) {
+
+  }
+
+  private static int search(int[] array, int key) {
+    for (int i = 0; i < array.length; i++) {
+
+    }
+  }
+}
